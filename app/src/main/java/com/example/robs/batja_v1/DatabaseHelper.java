79533d9be897996dea025d99e2fb472dd0e29a0f
Robs <vu16b001@technikum-wien.at>
2018-03-31 12:23:16 +0200

    updating stuff

--- app/src/main/java/com/example/robs/batja_v1/DatabaseHelper.java
+++ app/src/main/java/com/example/robs/batja_v1/DatabaseHelper.java
@@ -1,8 +1,14 @@
 package com.example.robs.batja_v1;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Robs on 30.03.18.
@@ -27,11 +33,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String KEY_GPS_ID = "id";
     private static final String KEY_GPS_USERS_ID_FK = "userId";
 
+    private static final String DB_FULL_PATH = "";
 
 
     private static DatabaseHelper sInstance;
-
-    // ...
 
     public static synchronized DatabaseHelper getInstance(Context context) {
         // Use the application context, which will ensure that you
@@ -76,6 +81,69 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        if (oldVersion != newVersion) {
+            // Simplest implementation is to drop all old tables and recreate them
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_GPS);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+            onCreate(db);
+        }
+
+    }
+
+    public long addOrUpdateUser(String userName, String password) {
+        // The database connection is cached so it's not expensive to call getWriteableDatabase() multiple times.
+        SQLiteDatabase db = getWritableDatabase();
+        long userId = -1;
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_USERS_NAME, userName);
+
+            // First try to update the user in case the user already exists in the database
+            // This assumes userNames are unique
+            int rows = db.update(TABLE_USERS, values, KEY_USERS_NAME + "= ?", new String[]{userName});
+
+            // Check if update succeeded
+            if (rows == 1) {
+                // Get the primary key of the user we just updated
+                String usersSelectQuery = String.format("SELECT %s FROM %s WHERE %s = ?",
+                        KEY_USERS_ID, TABLE_USERS, KEY_USERS_NAME);
+                Cursor cursor = db.rawQuery(usersSelectQuery, new String[]{String.valueOf(userName)});
+                try {
+                    if (cursor.moveToFirst()) {
+                        userId = cursor.getInt(0);
+                        db.setTransactionSuccessful();
+                    }
+                } finally {
+                    if (cursor != null && !cursor.isClosed()) {
+                        cursor.close();
+                    }
+                }
+            } else {
+                // user with this userName did not already exist, so insert new user
+                userId = db.insertOrThrow(TABLE_USERS, null, values);
+                db.setTransactionSuccessful();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to add or update user");
+        } finally {
+            db.endTransaction();
+        }
+        return userId;
+    }
+
+    private boolean checkDataBase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            checkDB = SQLiteDatabase.openDatabase()
+            checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null,
+                    SQLiteDatabase.OPEN_READONLY);
+            checkDB.close();
+        } catch (SQLiteException e) {
+            // database doesn't exist yet.
+        }
+        return checkDB != null;
     }
 
 }
--- app/src/main/java/com/example/robs/batja_v1/MainActivity.java
+++ app/src/main/java/com/example/robs/batja_v1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.robs.batja_v1;
 
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +13,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
+    private Button button2;
     private EditText textUserName;
     private EditText textUserPassword;
     String userName;
     String userPassword;
+
+    AlertDialog.Builder missingTextAlert = new AlertDialog.Builder(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1 = (Button) findViewById(R.id.buttonLogin);
+        button2 = (Button) findViewById(R.id.buttonNewHere);
         textUserName = (EditText) findViewById(R.id.userName);
         textUserPassword = (EditText) findViewById(R.id.userPWD);
 
@@ -34,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                handleButton2OnClick();
+
+            }
+        });
+
     }
 
     public void handleButton1OnClick(){
@@ -41,8 +57,28 @@ public class MainActivity extends AppCompatActivity {
         userName = textUserName.getText().toString();
         userPassword = textUserPassword.getText().toString();
 
-        Toast.makeText(MainActivity.this, "Name: " + userName + " , PWD: " + userPassword, Toast.LENGTH_SHORT);
+        Toast toast1 = Toast.makeText(MainActivity.this, "Name: " + userName + " , PWD: " + userPassword, Toast.LENGTH_SHORT);
+        toast1.show();
 
+        if(userName.isEmpty() || userPassword.isEmpty())
+        {
+            showAlert(View v, new String("You forgot either the name or the password"));
+        }
+
+    }
+
+    public void handleButton2OnClick(){
+
+        userName = textUserName.getText().toString();
+        userPassword = textUserPassword.getText().toString();
+
+    }
+
+    public void showAlert(View v, String message){
+
+        missingTextAlert.setMessage(message)
+                .create()
+                .show();
 
 
     }
